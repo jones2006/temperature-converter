48cb8e7fff6a1668bd7a483fabf1cf48c1c76650
jones.j <jones.j8637@gmail.com>
2025-06-09 20:36:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-
-// alert("this is a converter")
+alert("Temperature converter");
 
 function convert() {
   let txt_field = Number(document.getElementById("txt_field").value);
   let fahrenval = (txt_field * 9) / 5 + 32;
+  let celcuis = ((txt_field - 32) * 5) / 9;
   let result = document.getElementById("result");
-  result.innerHTML = fahrenval + " fahrenheit";
+
+  const conversion = document.getElementById("conversion").value;
+
+  if (conversion == "ftoc") {
+    result.innerHTML = `${txt_field}°F = ${celcuis.toFixed(2)}°C ❄️`;
+  } else {
+    result.innerHTML = `${txt_field}°C = ${fahrenval.toFixed(2)}°F 🔥`;
+  }
 }
